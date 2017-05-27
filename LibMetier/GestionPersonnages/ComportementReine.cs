bff7d7c503ece778e9d3a899422669b2a1006f82
champCAR <gagoudechris@gmail.com>
2017-05-27 14:53:41 +0100

    Commit

--- LibMetier/GestionPersonnages/ComportementReine.cs
+++ LibMetier/GestionPersonnages/ComportementReine.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibAbstraite;
 
 namespace LibMetier
 {
@@ -14,7 +15,55 @@ namespace LibMetier
 
         public override Fourmi CreerFourmi()
         {
-            return base.CreerFourmi();
+            Fourmi nouvelleFourmi = new Fourmi();
+            nouvelleFourmi.Etat.IdFlag = (int)EtatFlags.EtatOeuf;
+            nouvelleFourmi.PointDeVie = 1;
+            nouvelleFourmi.Comportement = new ComportementVide(nouvelleFourmi);
+
+            return nouvelleFourmi;
         }
+
+        public override void MangerObjet(ObjetAbstrait unObjet)
+        {
+            base.MangerObjet(unObjet);
+        }
+
+        public override void Nourrir(Fourmi fourmi)
+        {
+            
+        }
+
+        public override void Avancer()
+        {
+            base.Avancer();
+        }
+
+        public override void Mourir()
+        {
+            base.Mourir();
+        }
+
+        public override void ChoixZoneSuivante(ZoneAbstraite zone)
+        {
+            base.ChoixZoneSuivante(zone);
+        }
+
+        public override void Combattre(PersonnageAbstrait ennemi)
+        {
+            Console.WriteLine("Opération non autorisée");
+        }
+
+        public override void Creuser(ZoneAbstraite uneZone)
+        {
+            Console.WriteLine("Opération non autorisée");
+        }
+
+        public override void DeposerPheromone(ZoneAbstraite uneZone)
+        {
+            Console.WriteLine("Opération non autorisée");
+        }
+
+        public override 
+
     }
 }
---
+++ LibMetier/GestionPersonnages/ComportementVide.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMetier
+{
+    public class ComportementVide : ComportementFourmi
+    {
+        public ComportementVide(Fourmi fourmi) : base(fourmi)
+        {
+        }
+    }
+}
